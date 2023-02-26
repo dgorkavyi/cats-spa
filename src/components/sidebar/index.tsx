bd6db2f0dd6516ba664102d25cdb8337b9329077
Dmytro <dima.net.dd@gmail.com>
2023-02-26 13:45:09 +0200

    fix: rewrite component styles to tailwind

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -19,28 +19,41 @@ function SideBarLinks() {
     return () => setStyle({ ...defaultState, [name]: true });
   }
 
+  const votingStyle = style.voting ? styles.nav__btn_active : styles.nav__btn;
+  const breedsStyle = style.breeds ? styles.nav__btn_active : styles.nav__btn;
+  const galleryStyle = style.gallery ? styles.nav__btn_active : styles.nav__btn;
+
   return (
     <>
       <NavLink
         to={routes.voting}
+        className={styles.navlink_hover}
         onClick={activateLink("voting")}
-        className={style.voting ? styles.nav__btn_active : styles.nav__btn}
       >
-        VOTING
+        <div className={styles.nav__element_voting}>
+          <img src={nav__img_voting} alt="nav img" />
+        </div>
+        <div className={votingStyle}>VOTING</div>
       </NavLink>
       <NavLink
         to={routes.breeds}
+        className={styles.navlink_hover}
         onClick={activateLink("breeds")}
-        className={style.breeds ? styles.nav__btn_active : styles.nav__btn}
       >
-        BREEDS
+        <div className={styles.nav__element_breeds}>
+          <img src={nav__img_breeds} alt="nav img" />
+        </div>
+        <div className={breedsStyle}>BREEDS</div>
       </NavLink>
       <NavLink
         to={routes.gallery}
+        className={styles.navlink_hover}
         onClick={activateLink("gallery")}
-        className={style.gallery ? styles.nav__btn_active : styles.nav__btn}
       >
-        GALLERY
+        <div className={styles.nav__element_gallery}>
+          <img src={nav__img_gallery} alt="nav img" />
+        </div>
+        <div className={galleryStyle}>GALLERY</div>
       </NavLink>
     </>
   );
@@ -51,17 +64,6 @@ export default function SideBar() {
     <div className={styles.sidebar}>
       <img className={styles.logo} src={logo} alt="logo" />
       <p className={styles.sidebar__text}>Lets start using The Cat API</p>
-      <div className={styles.nav}>
-        <div className={styles.nav__element_voting}>
-          <img src={nav__img_voting} alt="nav img" />
-        </div>
-        <div className={styles.nav__element_breeds}>
-          <img src={nav__img_breeds} alt="nav img" />
-        </div>
-        <div className={styles.nav__element_gallery}>
-          <img src={nav__img_gallery} alt="nav img" />
-        </div>
-      </div>
       <div className={styles.nav}>
         <SideBarLinks />
       </div>
