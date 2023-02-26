eeba1595484ceda595c095962f9583fef6f2ad19
Dmytro <dima.net.dd@gmail.com>
2023-02-26 12:43:47 +0200

    feat: add index page

--- src/components/index/index.tsx
+++ src/components/index/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./style.module.scss";
+import img from "../../images/girl-and-pet.png";
 
 export default function Index() {
   return (
     <div className={styles.index}>
-      <p>INDEX</p>
+      <img src={img} alt="girl_and_pet_image" />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      indexcolor: "#FBE0DC",
+    },
     extend: {},
   },
   plugins: [],
