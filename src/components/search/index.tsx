2562309407d3f54e8b787a70f4f8ee11c2a9e699
Dmytro <dima.net.dd@gmail.com>
2023-02-27 16:57:45 +0200

    feat: add search component

---
+++ src/components/search/index.tsx
@@ -0,0 +1,41 @@
+import React, { useRef, FormEvent } from "react";
+import styles from "./style.module.scss";
+import { ReactComponent as ImgHeart } from "../../images/heart.svg";
+import { ReactComponent as ImgLike } from "../../images/like.svg";
+import { ReactComponent as ImgDislike } from "../../images/dislike.svg";
+import { ReactComponent as ImgSearch } from "../../images/search.svg";
+
+export default function Search() {
+  const searchRef = useRef<HTMLInputElement>(null);
+  const search = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("searchRef.current :>>", searchRef.current?.value);
+  };
+
+  return (
+    <div className={styles.search}>
+      <div className={styles.input}>
+        <input
+          ref={searchRef}
+          placeholder="Search for breeds by name"
+          type="search"
+          name="search"
+          id="searhc"
+        />
+        <button type="submit" className={styles.search_btn} onClick={search}>
+          <ImgSearch />
+        </button>
+      </div>
+
+      <div className={styles.btn}>
+        <ImgLike className={styles.svg} />
+      </div>
+      <div className={styles.btn}>
+        <ImgHeart className={styles.svg} />
+      </div>
+      <div className={styles.btn}>
+        <ImgDislike className={styles.svg} />
+      </div>
+    </div>
+  );
+}
--- src/components/voting/index.tsx
+++ src/components/voting/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Search from "../search";
 import styles from "./style.module.scss";
 
 export default function Voting() {
   return (
     <div className={styles.voting}>
-      <p>VOTING</p>
+      <Search />
     </div>
   );
 }
